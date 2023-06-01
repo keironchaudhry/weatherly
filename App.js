955e469f6e6dfd8fa6ba9c9af94f1d567d284346
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-06-01 10:18:47 +0200

    Fix: Remove API key

--- App.js
+++ App.js
@@ -11,11 +11,10 @@ import {
 import axios from "axios";
 
 export default function App() {
-  const APIKey = a293bc6acc6834526730bb1203942cec;
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
 
   const searchLocation = (event) => {
     axios.get(url).then((response) => {
